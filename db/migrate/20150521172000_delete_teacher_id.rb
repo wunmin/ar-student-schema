b2d5a3e10d7abc592a37a9bb0de8db73693c59ef
AmiruAsyraf <amiruasyraf@gmail.com>
2015-05-21 17:55:10 +0800

    Change DeleteTeacherID to DeleteTeacherId as it was stopping migration

--- db/migrate/20150521172000_delete_teacher_id.rb
+++ db/migrate/20150521172000_delete_teacher_id.rb
@@ -2,10 +2,10 @@ require_relative '../config'
 
 # this is where you should use an ActiveRecord migration to
 
-class DeleteTeacherID < ActiveRecord::Migration
+class DeleteTeacherId < ActiveRecord::Migration
   def change
-    change_table :students do |student|
-      student.remove :teacher_id
+    change_table :students do |s|
+      s.remove :teacher_id
     end
   end
 end
--- db/migrate/20150521172400_create_students_teachers.rb
+++ db/migrate/20150521172400_create_students_teachers.rb
@@ -2,8 +2,7 @@ require_relative '../config'
 
 # this is where you should use an ActiveRecord migration to
 
-class CreateTeacherStudent <
-  ActiveRecord::Migration
+class CreateStudentsTeachers < ActiveRecord::Migration
   def change
     create_table :student_teachers do |s|
       s.integer :student_id
